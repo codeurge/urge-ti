e7354eec52a7c331f8c11de7a7a37fa04b18d3e6
Derek Keith <derek@codeurge.com>
2016-02-26 00:02:13 -0700

    Modify settings and generate bundle.js for github.io

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './index.js',
   publicPath: '/public/',
-  output: { path: __dirname + '/build', filename: 'bundle.js' },
+  output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
       {
